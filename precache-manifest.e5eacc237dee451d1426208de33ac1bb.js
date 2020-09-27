80b4193460ec0b609eb45bffe93840e3ae49df73
C4PT4INNULL <chaudharyarun5797@gmail.com>
2020-09-27 20:10:53 +0530

    initital

---
+++ precache-manifest.e5eacc237dee451d1426208de33ac1bb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cadc895d6b84a2d616bfd2d48a69ce4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb8aa5dc8dfc49c1670f",
+    "url": "/static/css/main.a5e15c68.chunk.css"
+  },
+  {
+    "revision": "8858747af779775658ee",
+    "url": "/static/js/2.f23f7080.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f23f7080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb8aa5dc8dfc49c1670f",
+    "url": "/static/js/main.c3b6c75d.chunk.js"
+  },
+  {
+    "revision": "e475f5ed950b2890682d",
+    "url": "/static/js/runtime-main.d775e4e0.js"
+  },
+  {
+    "revision": "efcd3a17efddf24036570602eac4b6f2",
+    "url": "/static/media/ig_icon.efcd3a17.png"
+  },
+  {
+    "revision": "0a1cf2afec1ca331d4127e8bac4e6f07",
+    "url": "/static/media/image.0a1cf2af.gif"
+  },
+  {
+    "revision": "dc080759d6ed077bb2bd5f37b17c1120",
+    "url": "/static/media/port.dc080759.gif"
+  }
+]);
